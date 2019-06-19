c28bc146deff8846db28908ce1f95689a91969cf
Gareth Evans <hello@garethevans.co>
2019-06-19 16:21:43 +0100

    Committed files

---
+++ app/src/main/java/co/garethevans/mediaplayer/MusicController.java
@@ -0,0 +1,16 @@
+package co.garethevans.mediaplayer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c) {
+        super(c);
+    }
+
+    public void hide() {
+    }
+
+}
